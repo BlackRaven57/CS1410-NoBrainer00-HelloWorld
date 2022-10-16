2f08a5460f410149962859157c4c1fd2b04add5d
BlackRaven57 <89608631+BlackRaven57@users.noreply.github.com>
2022-10-16 12:06:30 -0600

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,18 @@
+// Hello World
+// CS 1410
+// Ellie Moore
+// August 31, 2022
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+  
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+  cout << "You typed " << num << endl;
+}
